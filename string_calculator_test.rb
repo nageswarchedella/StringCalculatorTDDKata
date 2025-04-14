d51761e4aa9e6a0646370de8930fc4927d6fed58
Nageswar Chedella <nageswarchedella@gmail.com>
2025-04-14 23:33:07 +0530

    Add test case for two numbers in StringCalculator

--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -8,4 +8,8 @@ class StringCalculatorTest < Minitest::Test
   def test_single_number
     assert_equal 1, StringCalculator.add("1")
   end
+
+  def test_two_numbers
+    assert_equal 3, StringCalculator.add("1,2")
+  end
 end
