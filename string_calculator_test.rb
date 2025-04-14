05f3b23c4b9d2e36c089324c65cf5d3add3d9d95
Nageswar Chedella <nageswarchedella@gmail.com>
2025-04-14 23:30:48 +0530

    Add test case for single number input in StringCalculator

--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -4,4 +4,8 @@ class StringCalculatorTest < Minitest::Test
   def test_empty_string
     assert_equal 0, StringCalculator.add("")
   end
+
+  def test_single_number
+    assert_equal 1, StringCalculator.add("1")
+  end
 end
