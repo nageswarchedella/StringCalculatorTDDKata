4f7c5ed86fe7268ba55f4b41222e71cc9b010c07
Nageswar Chedella <nageswarchedella@gmail.com>
2025-04-15 01:02:35 +0530

    Refactor StringCalculator methods for improved readability and consistency

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,33 +1,35 @@
 class StringCalculator
-  def self.add(string)
-    return 0 if string.empty?
+  class << self
+    def add(string)
+      return 0 if string.empty?
 
-    delimiter, numbers_string = extract_delimiter_and_numbers(string)
-    numbers = numbers_string.split(delimiter).map(&:to_i)
+      delimiter, numbers_string = extract_delimiter_and_numbers(string)
+      numbers = numbers_string.split(delimiter).map(&:to_i)
 
-    raise_error_if_negatives_present(numbers)
+      raise_error_if_negatives_present(numbers)
 
-    numbers.select { |num| num <= 1000 }.sum
-  end
-
-  def self.extract_delimiter_and_numbers(string)
-    delimiter = /[\n,]/
-    if string.start_with?('//')
-      delimiter_part, numbers_part = string.split("\n", 2)
-      custom_delimiters = delimiter_part[2..].scan(/\[(.*?)\]/).flatten
-
-      delimiter = if custom_delimiters.any?
-                    Regexp.union(custom_delimiters)
-                  else
-                    Regexp.new(Regexp.escape(delimiter_part[2..]))
-                  end
-      return delimiter, numbers_part
+      numbers.select { |num| num <= 1000 }.sum
     end
-    [delimiter, string]
-  end
 
-  def self.raise_error_if_negatives_present(numbers)
-    negatives = numbers.select(&:negative?)
-    raise "negative numbers not allowed #{negatives.join(',')}" if negatives.any?
+    def extract_delimiter_and_numbers(string)
+      delimiter = /[\n,]/
+      if string.start_with?('//')
+        delimiter_part, numbers_part = string.split("\n", 2)
+        custom_delimiters = delimiter_part[2..].scan(/\[(.*?)\]/).flatten
+
+        delimiter = if custom_delimiters.any?
+                      Regexp.union(custom_delimiters)
+                    else
+                      Regexp.new(Regexp.escape(delimiter_part[2..]))
+                    end
+        return delimiter, numbers_part
+      end
+      [delimiter, string]
+    end
+
+    def raise_error_if_negatives_present(numbers)
+      negatives = numbers.select(&:negative?)
+      raise "negative numbers not allowed #{negatives.join(',')}" if negatives.any?
+    end
   end
 end
