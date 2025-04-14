f8a67a9737b3ca21774f606bbc1725472de4d0be
Nageswar Chedella <nageswarchedella@gmail.com>
2025-04-14 23:43:32 +0530

    Refactor add method to support custom delimiters in StringCalculator

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,6 +1,12 @@
 class StringCalculator
   def self.add(string)
     0 if string.empty?
-    string.split(/[\n,]/).map(&:to_i).sum
+    if string.start_with?("//")
+      delimiter = string[2]
+      string = string[4..-1]
+    else
+      delimiter = /[\n,]/
+    end
+    string.split(delimiter).map(&:to_i).sum
   end
 end
