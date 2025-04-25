acbd12bcf243245a013c697c272e446573ac449c
Nageswar Chedella <nageswarchedella@gmail.com>
2025-04-25 16:09:51 +0530

    Refactor StringCalculator and StringParser for improved structure and readability

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,35 +1,48 @@
 class StringCalculator
   class << self
     def add(string)
-      return 0 if string.empty?
-
-      delimiter, numbers_string = extract_delimiter_and_numbers(string)
-      numbers = numbers_string.split(delimiter).map(&:to_i)
-
-      raise_error_if_negatives_present(numbers)
-
+      parser = StringParser.new(string)
+      numbers = parser.parse
+      Validator.validate(numbers)
       numbers.select { |num| num <= 1000 }.sum
     end
+  end
+end
 
-    def extract_delimiter_and_numbers(string)
-      delimiter = /[\n,]/
-      if string.start_with?('//')
-        delimiter_part, numbers_part = string.split("\n", 2)
-        custom_delimiters = delimiter_part[2..].scan(/\[(.*?)\]/).flatten
+class StringParser
+  def initialize(string)
+    @string = string
+  end
 
-        delimiter = if custom_delimiters.any?
-                      Regexp.union(custom_delimiters)
-                    else
-                      Regexp.new(Regexp.escape(delimiter_part[2..]))
-                    end
-        return delimiter, numbers_part
-      end
-      [delimiter, string]
+  def parse
+    return [] if @string.empty?
+
+    delimiter, numbers_string = extract_delimiter_and_numbers
+    numbers_string.split(delimiter).map(&:to_i)
+  end
+
+  private
+
+  def extract_delimiter_and_numbers
+    delimiter = /[\n,]/
+    if @string.start_with?('//')
+      delimiter_part, numbers_part = @string.split("\n", 2)
+      custom_delimiters = delimiter_part[2..].scan(/\[(.*?)\]/).flatten
+
+      delimiter = if custom_delimiters.any?
+                    Regexp.union(custom_delimiters)
+                  else
+                    Regexp.new(Regexp.escape(delimiter_part[2..]))
+                  end
+      return delimiter, numbers_part
     end
+    [delimiter, @string]
+  end
+end
 
-    def raise_error_if_negatives_present(numbers)
-      negatives = numbers.select(&:negative?)
-      raise "negative numbers not allowed #{negatives.join(',')}" if negatives.any?
-    end
+class Validator
+  def self.validate(numbers)
+    negatives = numbers.select(&:negative?)
+    raise "negative numbers not allowed #{negatives.join(',')}" if negatives.any?
   end
 end
